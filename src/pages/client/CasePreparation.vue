05d936a7a13bcbbe041c8ab5e05696d1b6c8ed39
AQUACY <godwinboadi@gmail.com>
2025-03-19 16:56:27 +0000

    final update as per the client instructions

--- src/pages/client/CasePreparation.vue
+++ src/pages/client/CasePreparation.vue
@@ -16,7 +16,9 @@
         <q-card-section>
           <div class="text-h6 q-mb-md">Step 1</div>
 
-          <div class="text-green q-mb-sm">Please complete the Form I-140 Questionnaire.</div>
+          <div class="q-mb-sm">
+            Please complete <span class="text-green">the Form I-140 Questionnaire.</span>
+          </div>
 
           <div class="text-green q-mb-sm">
             Notify your Case Manager via message as soon as you have completed the Form I-140
@@ -27,9 +29,10 @@
             We review your Form I-140 Questionnaire and provide feedback.
           </div>
 
-          <div class="text-red">
-            Once we have reviewed and confirmed your information in the Form I-140 Questionnaire, we
-            will generate the Form I-140 for you to sign.
+          <div class="">
+            Once we have reviewed and confirmed your information in the
+            <span class="text-green">Form I-140 Questionnaire</span>,
+            <span class="text-red">we will generate the Form I-140 for you to sign.</span>
           </div>
 
           <div class="row justify-end q-mt-md">
@@ -67,17 +70,23 @@
         <q-card-section>
           <div class="text-h6 q-mb-md">Step 3 (Optional depending on case evaluation)</div>
 
-          <div class="q-mb-md">Please provide your list of recommenders.</div>
+          <div class="q-mb-md text-green">Please provide your list of recommenders.</div>
 
           <div class="text-red q-mb-md">
-            We will review your list of recommenders and provide feedback. After your list of
-            recommenders is complete, we will draft recommendation letter templates based on your
-            profile and proposed endeavor and send them to you for review.
+            <span class="text-red"
+              >We will review your list of recommenders and provide feedback.</span
+            >
+            <br />
+            After your list of recommenders is complete,
+            <span class="text-red"
+              >we will draft recommendation letter templates based on your profile and proposed
+              endeavor and send them to you for review.</span
+            >
           </div>
 
-          <div class="text-green q-mb-md">
-            After the templates are finalized, please send them to your recommenders and ask them
-            to:
+          <div class="q-mb-md">
+            After the templates are finalized, please send
+            <span class="text-green">them to your recommenders and ask them to:</span>
             <ol>
               <li>Edit the templates as they see fit</li>
               <li>Print the letter on their institutional letterhead</li>
@@ -96,8 +105,8 @@
           <div class="text-h6 q-mb-md">Step 4</div>
 
           <div class="text-green q-mb-md">
-            Whilst waiting for the recommendation letters, please complete the Personal Statement
-            section.
+            Whilst waiting for the recommendation letters, please complete the
+            <span class="text-green">Personal Statement section.</span>
           </div>
 
           <div class="text-red">We will review your Personal Statement and provide feedback.</div>
@@ -117,10 +126,13 @@
         <q-card-section>
           <div class="text-h6 q-mb-md">Step 5 (Only for clients with foreign degrees)</div>
 
-          <div class="text-green">
+          <div class="">
             If you hold a foreign degree, a degree evaluation is required to confirm its equivalency
-            to a U.S. advanced degree. We recommend that you use Trustforte for your credential
-            evaluation. <a href="http://www.trustforte.com" target="_blank">www.trustforte.com</a>
+            to a U.S. advanced degree.
+            <span class="text-green"
+              >We recommend that you use Trustforte for your credential evaluation.</span
+            >
+            <a href="http://www.trustforte.com" target="_blank">www.trustforte.com</a>
           </div>
 
           <div class="text-body2 q-mt-sm">
@@ -135,8 +147,9 @@
         <q-card-section>
           <div class="text-h6 q-mb-md">Step 6</div>
 
-          <div class="text-green">
-            Please upload all the supporting documents to your Document tab.
+          <div class="">
+            Please upload
+            <span class="text-green">all the supporting documents to your Document tab.</span>
           </div>
 
           <div class="text-red">We start to assemble your evidence package online.</div>
@@ -166,7 +179,7 @@
 
           <div class="text-red q-mb-md">We will draft and assemble your petition package.</div>
 
-          <div class="text-red q-mb-md">
+          <div class="q-mb-md">
             Your Case Manager will draft the petition, compile evidence and ensure that all
             materials meet USCIS standards.
           </div>
@@ -203,13 +216,15 @@
 
           <div class="text-red q-mb-md">
             Once the application passes the legal review, our team prints, assembles, and packages
-            your documents for mailing to USCIS. You will receive your receipt notice from USCIS
-            within 30 days.
+            your documents for mailing to USCIS.
           </div>
 
           <div class="text-body2">
-            You can see the status of your case on the "Case Status" tab.
+            You will receive your receipt notice from USCIS within 30 days.
           </div>
+          <br />
+
+          <div class="q-mb-md">You can see the status of your case on the "Case Status" tab.</div>
 
           <div class="row justify-end q-mt-md">
             <q-btn color="primary" label="View Case Status" @click="goToCaseStatus" />
